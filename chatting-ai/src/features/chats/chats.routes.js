df619c35824375d8c0d7b2c111060221c0422e97
amaykorade <amaykorade5@gmail.com>
2025-03-22 19:56:48 +0530

    Added the cosine functionality and model

--- chatting-ai/src/features/chats/chats.routes.js
+++ chatting-ai/src/features/chats/chats.routes.js
@@ -1,10 +1,12 @@
 import express from 'express';
-import { getChats, addMessage } from './chats.controller.js';
+import { getChats, addMessage, getChatCosineScore } from './chats.controller.js';
 import jwtAuth from '../../middleware/jwt.middleware.js';
 
 const chatRouter = express.Router();
 
-chatRouter.get("/:room", jwtAuth, getChats);
+chatRouter.get("/:roomid", jwtAuth, getChats);
+
+chatRouter.get("/cosine/:roomid", getChatCosineScore);
 
 chatRouter.post("/", addMessage);
 
